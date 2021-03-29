f00cfcd526a756785ab9d134a0ea65978d82f6e4
Omkar Jadhav <omkarjadhav151@gmail.com>
2021-03-30 01:02:17 +0530

    some middleware added

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const mongoDBSanitize = require('express-mongo-sanitize');
+const httpParameterPolution = require('hpp');
 const app = express();
 const errorMiddleware = require('./middleware/error');
 const AppError = require("./util/error/appError");
-app.use(express.json());
+app.use(express.json({limit: "10kb"}));
+app.use(helmet());
+app.use(xss());
+app.use(mongoDBSanitize());
+app.use(httpParameterPolution({
+    whitelist:[]
+}))
 const demo = require('./routes/demo');
 app.use('/v1/demo',demo);
 
